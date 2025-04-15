7fb44fe63b22ae0d043e12fb58f028a58f4d7b2c
iaschowrai <iaschowrai@gmail.com>
2025-04-14 22:05:04 -0400

    BookMyShow class is created using Singleton design pattern

--- src/main/java/org/example/main/BookMyShow.java
+++ src/main/java/org/example/main/BookMyShow.java
@@ -5,6 +5,7 @@ import org.example.services.BookingService;
 public class BookMyShow {
     public static void main(String[] args) {
         BookingService bookingService = BookingService.getInstance();
-
+        bookingService.initialize();
+        bookingService.startBookingSession();
     }
 }
