e45a31669962acb57ec46ed966333f14624703e8
iaschowrai <iaschowrai@gmail.com>
2025-04-15 22:44:30 -0400

    Payment service contains the demo process of payment service

---
+++ src/main/java/org/example/services/PaymentService.java
@@ -0,0 +1,9 @@
+package org.example.services;
+
+public class PaymentService {
+    public boolean processPayment(double amount) {
+        System.out.println("Processing payment of ₹" + amount + "...");
+        // Simulate success
+        return true;
+    }
+}
